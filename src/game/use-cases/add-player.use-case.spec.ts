1d7ad7fd10a0a1171a5b6fd7d35e423f04930b54
Lucas <ferreira.lucasalves@gmail.com>
2024-01-29 14:34:36 -0300

    feat: create use-case for add players in a game

---
+++ src/game/use-cases/add-player.use-case.spec.ts
@@ -0,0 +1,38 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { Game } from '../entities/game.entity';
+import { AddPlayerUseCase } from './add-player.use-case';
+
+describe('AddPlayerUseCase', () => {
+  let game: Game;
+  let addPlayerUseCase: AddPlayerUseCase;
+
+  beforeEach(async () => {
+    game = { players: [], kills: {} } as Game;
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [Game, AddPlayerUseCase],
+    }).compile();
+
+    game = module.get<Game>(Game);
+    addPlayerUseCase = module.get<AddPlayerUseCase>(AddPlayerUseCase);
+
+    addPlayerUseCase = new AddPlayerUseCase();
+    addPlayerUseCase.setGame(game);
+  });
+
+  it('should add a new player if not already in the game', () => {
+    const player = 'Player1';
+    addPlayerUseCase.execute(player);
+
+    expect(game.players).toContain(player);
+    expect(game.kills[player]).toBe(0);
+  });
+
+  it('should not add a player if they are already in the game', () => {
+    const player = 'Player1';
+    addPlayerUseCase.execute(player);
+    addPlayerUseCase.execute(player);
+
+    expect(game.players.filter((p) => p === player).length).toBe(1);
+  });
+});
---
+++ src/game/use-cases/add-player.use-case.ts
@@ -0,0 +1,17 @@
+import { Game } from '../entities/game.entity';
+import { IAddPlayerUseCase } from './interfaces/add-player.use-case.interface';
+
+export class AddPlayerUseCase implements IAddPlayerUseCase {
+  private game: Game;
+
+  public setGame(game: Game): void {
+    this.game = game;
+  }
+
+  public execute(player: string): void {
+    if (!this.game.players.includes(player)) {
+      this.game.players.push(player);
+      this.game.kills[player] = this.game.kills[player] || 0;
+    }
+  }
+}
