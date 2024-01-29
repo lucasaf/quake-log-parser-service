1f79c6764b938b10674dfc0abd99a229fe7b99c1
Lucas <ferreira.lucasalves@gmail.com>
2024-01-29 14:40:27 -0300

    feat: create CLI module

--- src/cli/cli.module.ts
+++ src/cli/cli.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { FileWriterService } from '../file/file-writer/file-writer.service';
+import { FileModule } from '../file/file.module';
+import { LogParserModule } from '../log-parser/log-parser.module';
 import { CliService } from './services/cli.service';
 
 @Module({
-  providers: [CliService],
+  imports: [LogParserModule, FileModule],
+  providers: [CliService, FileWriterService],
   exports: [CliService],
 })
 export class CliModule {}
