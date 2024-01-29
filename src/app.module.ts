55b8a58513700172d49d33d6a9cc5425725b3264
Lucas <ferreira.lucasalves@gmail.com>
2024-01-29 14:43:20 -0300

    feat: import all modules created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CliModule } from './cli/cli.module';
+import { FileModule } from './file/file.module';
+import { GameModule } from './game/game.module';
+import { LogParserModule } from './log-parser/log-parser.module';
 
 @Module({
-  imports: [CliModule],
-  controllers: [AppController],
+  imports: [CliModule, FileModule, GameModule, LogParserModule],
   providers: [AppService],
 })
 export class AppModule {}
