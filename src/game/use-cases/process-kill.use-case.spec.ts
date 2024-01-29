895f32b7e7b597e5a0b9807783f053976b7d4f3e
Lucas <ferreira.lucasalves@gmail.com>
2024-01-29 19:28:11 -0300

    feat: add more test scenarios

--- src/game/use-cases/process-kill.use-case.spec.ts
+++ src/game/use-cases/process-kill.use-case.spec.ts
@@ -40,4 +40,52 @@ describe('ProcessKillUseCase', () => {
     expect(game.kills[killed]).toBe(0);
     expect(game.killsByMeans[mod]).toBe(1);
   });
+
+  it('should track multiple kills by the same player', () => {
+    const killer = 'Player3';
+    const killed = ['Player1', 'Player2'];
+    const mod = 'MOD_SHOTGUN';
+
+    killed.forEach((victim) => {
+      processKillUseCase.execute(killer, victim, mod);
+    });
+
+    expect(game.kills[killer]).toBe(killed.length);
+    expect(game.killsByMeans[mod]).toBe(killed.length);
+  });
+
+  it('should not increment killer score when <world> kills', () => {
+    const killer = '<world>';
+    const killed = 'Player4';
+    const mod = 'MOD_LAVA';
+
+    processKillUseCase.execute(killer, killed, mod);
+
+    expect(game.kills[killer]).toBeUndefined();
+    expect(game.killsByMeans[mod]).toBe(1);
+  });
+
+  it('should correctly track deaths by different MODs', () => {
+    processKillUseCase.execute('Player5', 'Player6', 'MOD_PLASMA');
+    processKillUseCase.execute('Player5', 'Player7', 'MOD_GRENADE');
+
+    expect(game.killsByMeans['MOD_PLASMA']).toBe(1);
+    expect(game.killsByMeans['MOD_GRENADE']).toBe(1);
+  });
+
+  it('should correctly track kills when the same player is killed by different killers', () => {
+    const killed = 'Player8';
+    const killers = ['Player9', 'Player10', 'Player11'];
+    const mod = 'MOD_MACHINEGUN';
+
+    killers.forEach((killer) => {
+      processKillUseCase.execute(killer, killed, mod);
+    });
+
+    expect(game.totalKills).toBe(killers.length);
+    killers.forEach((killer) => {
+      expect(game.kills[killer]).toBe(1);
+    });
+    expect(game.killsByMeans[mod]).toBe(killers.length);
+  });
 });
